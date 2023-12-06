b08547bc67e197673d971d92ead7480463ea77f3
luk3ya0 <oneTOinf@163.com>
2023-12-06 22:41:10 +0800

    prepare for apiv3 native

--- src/test/java/com/luke/payment/SpringPaymentApplicationTests.java
+++ src/test/java/com/luke/payment/SpringPaymentApplicationTests.java
@@ -1,13 +1,21 @@
 package com.luke.payment;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringPaymentApplicationTests {
 
+
+    /*
+     *
+    @Resource
+    private WxPayConfig wxPayConfig;
+
     @Test
-    void contextLoads() {
-    }
+    void testGetPrivateKey() {
+        String privateKeyPath = wxPayConfig.getPrivateKeyPath();
+        PrivateKey privateKey = wxPayConfig.getPrivateKey(privateKeyPath);
+        System.out.println(privateKey);
+    }*/
 
 }
